3c985f70a39227a6b87e4a797fc808c5be1784ad
euDyana <dyana.lourenzo@gmail.com>
2025-06-11 23:01:41 -0300

    Removendo import repetidos.

--- Projeto/Graficos/graficos.py
+++ Projeto/Graficos/graficos.py
@@ -1,9 +1,6 @@
 import plotly.express as px  # type: ignore
 import streamlit as st  # type: ignore
 import pandas as pd    # type: ignore
-import streamlit as st  # type: ignore
-import pandas as pd  # type: ignore
-import plotly.express as px  # type: ignore
 
 
 def acidente_mes(df):
